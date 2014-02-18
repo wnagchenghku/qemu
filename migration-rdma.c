8563d7c874e0a72f1d1aa6776bb3aa4e7c11b9fb
Michael R. Hines <mrhines@us.ibm.com>
2014-02-18 10:14:12 +0800

    fix memory leak

--- migration-rdma.c
+++ migration-rdma.c
@@ -2512,8 +2512,10 @@ static void *qemu_rdma_data_init(const char *host_port, Error **errp)
         } else {
             ERROR(errp, "bad RDMA migration address '%s'", host_port);
             g_free(rdma);
-            return NULL;
+            rdma = NULL;
         }
+
+        qapi_free_InetSocketAddress(addr);
     }
 
     return rdma;
